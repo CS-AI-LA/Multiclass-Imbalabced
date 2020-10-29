2d8e713ea955fb517ed4f90b02d1564e9fc2c433
CS-AI-LA <73648615+CS-AI-LA@users.noreply.github.com>
2020-10-29 17:25:23 +0330

    Delete Normalize.m

--- Normalize.m
+++
@@ -1,7 +0,0 @@
-function [NormalData,M] = Normalize(imbalancedDataset)
-% imbalancedDataset is all samples without labels
-% The NormalData is zero mean
-% M is the mean of the data
-
-NormalData = imbalancedDataset -repmat(mean(imbalancedDataset),[size(imbalancedDataset,1),1]); 
-M = mean(imbalancedDataset);
